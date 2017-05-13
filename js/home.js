e214bceaa1c1ae647eda5cb6191cdf0bb383e480
Tomasz Morawski <tomekm1700@gmail.com>
2017-05-13 21:33:34 +0200

    auth first approach

--- js/home.js
+++ js/home.js
@@ -1,14 +1,27 @@
 /**
  * Created by tomek on 13.05.2017.
  */
-define(() => {
+define(["vendor/axios"],(axios) => {
 "use strict";
 
 let component = {
     name:"il-home"
 };
 
-component.methods = {};
+component.methods = {
+    login () {
+        const url = "https://api.instagram.com/oauth/authorize/";
+        const client_id = "458a7a8e2850426699015a9e75813a43";
+        const redirect_uri = "https://tom1700.github.io/instagramfilter/";
+        axios.get(url,{
+            params: {
+                "client_id":client_id,
+                "redirect_uri":redirect_uri,
+                "response_type":"token"
+            }
+        });
+    }
+};
 
 component.data = () => { return {}; };
 
@@ -18,9 +31,7 @@ component.template = `
         <div class="flex-content">
             <h1>Witamy w aplikacji InstagramFilter</h1>
             <h2>Kliknij poniżej aby zalogować się na swoje konto</h2>
-            <router-link to="/images">
-                <button>Zaloguj</button>
-            </router-link>
+            <button v-on:click="login()">Zaloguj</button>
         </div>
     </div>
 </div>
