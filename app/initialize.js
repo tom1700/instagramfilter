c80af8e7e59078bfc2b38b81909652e4dd2dc63f
Tomasz Morawski <tomekm1700@gmail.com>
2017-06-21 15:21:53 +0200

    adding vision

--- app/initialize.js
+++ app/initialize.js
@@ -10,47 +10,25 @@ import VueRouter from 'vue-router';
 import routes from './config/routes';
 import PouchDB from "pouchdb";
 
-//
-// Vue extensions
-// Here we tell Vue that we want to add extra functionality to the application.
-//
 
 Vue.use(VueRouter);
+
 Vue.use({
     install: function(Vue, options) {
         Vue.prototype.$db = new PouchDB("auth");
     }
 });
 
-
-//
-// Enviromnment
-//
-
 const environment = process.env.NODE_ENV;
 
-//
-// Configure Vue
-// Here we will configure Vue based on the environment we are running in.
-//
-
 Vue.config.debug = (environment === 'development');
-Vue.config.devtools = (environment === 'development');
 
-//
-// Router
-// Here we will create the router instance.
-//
+Vue.config.devtools = (environment === 'development');
 
 let router = new VueRouter({
     mode: 'history',
     routes: routes
 });
-
-//
-// Start the application
-// Finally, we can start the application.
-//
 
 const app = new Vue({
     router,
